f568ba1b2a7b6e32fd86d88d4a8d936a6e2288e0
lecanyu <lecanyu@gmail.com>
2023-02-15 12:03:28 +0800

    minor1

--- adx_token.sol
+++ adx_token.sol
@@ -1,9 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.6;
 
-// import "@openzeppelin/contracts/token/ERC777/ERC777.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC777/ERC777.sol";
-
+import "@openzeppelin/contracts/token/ERC777/ERC777.sol";
 
 contract GLDToken is ERC777 {
     constructor(uint256 initialSupply, address[] memory defaultOperators)
---
+++ test_erc777_token.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.6;
+
+import "@openzeppelin/contracts/token/ERC777/ERC777.sol";
+
+contract GLDToken is ERC777 {
+    constructor(
+        uint256 initialSupply, 
+        address[] memory defaultOperators
+    ) ERC777("Gold", "GLD", defaultOperators)
+    {
+        _mint(msg.sender, initialSupply, "", "");
+    }
+}
+
+
